170a8a7568e2cd8b6fe92bb1510f1b432c8ae779
Daniel Rod <dsrod@seznam.cz>
2024-04-21 19:22:29 +0200

    remove flaky test

--- pubsub/test/test_pubsub.py
+++ pubsub/test/test_pubsub.py
@@ -215,6 +215,3 @@ class InProcessPubSubTest(unittest.IsolatedAsyncioTestCase):
         await yield_to_event_loop()
 
         task.cancel()
-
-    def test_flaky(self):
-        self.assertEqual(True, False)
